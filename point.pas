b15c831fe40da24e4cba73ebade61ecfeab71221
Toast <bored@toasted.me>
2024-10-07 09:49:42 +0400

    Add PointRevNum

---
+++ point.pas
@@ -0,0 +1,36 @@
+program point;
+type 
+  itemp = ^item;
+  item = record
+    data: integer;
+    next: itemp;
+  end;
+var
+  tmp,first:itemp;
+  n:integer;
+begin
+  first:= nil;
+  while true do
+  begin
+    writeln('Напиши циферку ');
+    read(n);
+    if n = 0 then
+    begin
+      break;
+    end;
+    new(tmp);
+    tmp^.data := n;
+    if first <> nil then
+      tmp^.next:= first
+    else
+      tmp^.next := nil;
+    first := tmp;
+  end;
+  while true do
+  begin
+    writeln(first^.data);
+    if first^.next = nil then
+      break;
+    first := first^.next;
+  end;
+end.
